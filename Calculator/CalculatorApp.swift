046f1ff8083930781c314baebb9fea5a39606291
fasoxy-it <fasoxy@gmail.com>
2021-04-08 23:00:04 +0200

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Mattia Fasoli on 08/04/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
